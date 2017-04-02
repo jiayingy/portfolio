0c9cb458672a23b03a3ce243578082ed8aca307c
jiayingy <jiayinglek@gmail.com>
2017-04-02 15:58:34 +0800

    Revamp portfolio

--- assets/scripts/scripts.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  
-  $(".projects-showcase ul li button").click(function() {
-  $(".projects-showcase ul li button").removeClass("active");
-  $(this).addClass("active");
-  });
-});
